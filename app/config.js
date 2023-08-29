a067bb7f58d0c93e2bfda79bb7f934e0843f38c2
Fabian Feichter <fabian.feichter.ext@porscheinformatik.com>
2023-08-29 12:33:28 +0200

    Revert "CHANGE: Config summer break for Mensa"
    This reverts commit 167a3d7cde2d579fdd4249c2fde1bacc95ea010c.

--- app/config.js
+++ app/config.js
@@ -11,7 +11,7 @@ module.exports = {
     onBreak: {
         //The dates are only rendered, never used for checking if closed!
         // mensa: new BreakInfo("Wir werden im Zeitraum", "1.6", "27.9", "renoviert und sind in Sommerpause.", "fa fa-sun"),
-        mensa: new BreakInfo("Liebe Gäste, unser Betrieb ist im Zeitraum", "7.8", "27.8", "geschlossen."),
+        // mensa: new BreakInfo("Wir befinden uns für den Zeitraum", "28.7", "2.9", "in Sommerpause."),
     },
     cache: {
         // Time in miliseconds -> 20 min.
